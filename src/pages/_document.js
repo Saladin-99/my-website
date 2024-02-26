0f3783c7528854fdf1d98e441d01c963142f68de
Saladin-99 <salahabdou99@gmail.com>
2024-02-26 04:19:33 +0200

    edited boilerplate structure and main sections

---
+++ src/pages/_document.js
@@ -0,0 +1,22 @@
+// src/pages/_document.js
+
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="My Website" content="website for recruiting" />
+          {/* Other meta tags, links, etc. */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;

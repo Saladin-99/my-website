e07645b7b4648683f9ad310a43a6459870e366ff
Saladin-99 <salahabdou99@gmail.com>
2024-02-28 06:33:57 +0200

    changes for deployement

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  experimental: {images: {unoptimized: true}},
+  basePath: "/my-website",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };

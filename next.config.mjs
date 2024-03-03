0488c98b0e1065067335da1ce3e24247564bce4d
Saladin-99 <salahabdou99@gmail.com>
2024-03-03 17:32:16 +0200

    uncommented lines in next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,9 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  //experimental: {images: {unoptimized: true}},                //these 3 configs are for gh pages deployment
-  //basePath: "/my-website",                                    //and need to be commented out to be able
-  //output: "export",  // <=== enables static exports           //to run locally with npm run  
+  experimental: {images: {unoptimized: true}},                //these 3 configs are for gh pages deployment
+  basePath: "/my-website",                                    //and need to be commented out to be able
+  output: "export",  // <=== enables static exports           //to run locally with npm run  
   reactStrictMode: true,
 };
   
